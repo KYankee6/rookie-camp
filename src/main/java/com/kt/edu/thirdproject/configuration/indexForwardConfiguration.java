40ed9efb0faffe44e93f49d6a1ed5bce5e1fd6cf
Kyankee6 <didzl1231@naver.com>
2022-09-26 03:11:43 +0900

    [add] set forward Configuration

--- src/main/java/com/kt/edu/thirdproject/configuration/indexForwardConfiguration.java
+++ src/main/java/com/kt/edu/thirdproject/configuration/indexForwardConfiguration.java
@@ -1,2 +1,16 @@
-package com.kt.edu.thirdproject.configuration;public class indexForwardConfiguration {
+package com.kt.edu.thirdproject.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class indexForwardConfiguration {
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addViewControllers(ViewControllerRegistry registry) {
+            registry.addViewController("/").setViewName("forward:/index.html");
+
+        }
+    }
 }
